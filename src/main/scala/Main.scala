53c5ef635c719798190a3c637fdfe13b344666a3
Richie Lee <richie.lee@itv.com>
2022-12-01 16:56:46 +0000

    added better solution

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-import scala.io.Source
+import scala.io.{BufferedSource, Source}
 import scala.util.{Failure, Success, Try}
 
 object AdventOfcode extends App {
@@ -7,30 +7,42 @@ object AdventOfcode extends App {
 
 object Resource {
   def forDay(day: Int) = {
-    Source.fromResource(s"day$day.txt").getLines()
+    Source.fromResource(s"day$day.txt")
   }
 }
 
 object Day1 {
+  def solveHindSight = {
+    val elvesByCalories =
+      Resource
+        .forDay(1)
+        .mkString("")
+        .split("\n\n")
+        .toList
+        .map(_.split("\n").map(_.toInt).sum)
+
+    val maxCalories = elvesByCalories.max
+    val top3 = elvesByCalories.sorted(Ordering.Int.reverse).take(3)
+    val top3Total = top3.sum
+
+    println(s"maxCalories: $maxCalories\ntop3: $top3\ntop3Total: $top3Total")
+  }
   def solve = {
-    val elfData = Resource.forDay(1)
+    val elfData = Resource.forDay(1).getLines()
+
     val elvesByCalories = elfData
       .foldLeft((0, List(0)))((indexAndList, maybeCalories) => {
         val currentValue = indexAndList._1
         val elfList = indexAndList._2
         Try(maybeCalories.toInt) match {
           case Success(value) => (currentValue + value, elfList)
-          case Failure(_)     =>
-            // turbo bad but im assuming a falure is a blank row - Ie. a new elf
-            (0, elfList ++ List(currentValue))
+          case Failure(_)     => (0, elfList ++ List(currentValue))
         }
       })
       ._2
 
     val maxCalories = elvesByCalories.max
-    val top3 = {
-      elvesByCalories.sorted(Ordering.Int.reverse).take(3)
-    }
+    val top3 = elvesByCalories.sorted(Ordering.Int.reverse).take(3)
     val top3Total = top3.sum
 
     println(s"maxCalories: $maxCalories\ntop3: $top3\ntop3Total: $top3Total")
