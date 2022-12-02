82fbbfadefcd3f486f13dbdc98e893e235635fef
Richie Lee <richie.lee@itv.com>
2022-12-02 09:20:46 +0000

    fixed resource script to make the directory for the new day

--- getResource.sh
+++ getResource.sh
@@ -4,4 +4,5 @@ DAY=$(date | awk '{print $3}')
 RESDIR="src/main/resource"
 COOKIE="session=[SETCOOKIEHERE]"
 
+mkdir -p $RESDIR/day$DAY
 curl --cookie $COOKIE https://adventofcode.com/2022/day/"$DAY"/input > $RESDIR/day$DAY/day$DAY.txt
